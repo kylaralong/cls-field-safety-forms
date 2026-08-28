b06525e526f52b39ab6687ed61ccd219d6808877
kylaralong <kylaralong1@gmail.com>
2026-08-29 08:56:46 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='cls-forms-master-v6';
-const ASSETS=['/','/index.html','/manifest.webmanifest','/logo.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('/index.html'))))});
